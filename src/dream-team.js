851792754f617bb73ebb850fc4984e745f8b7bb1
Masha <valzyr48@gmail.com>
2020-02-21 16:43:22 +0300

    feat: add solution dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,12 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
+module.exports = function createDreamTeam(members) {
+  let result = [];
+  if (members instanceof Array) {
+    members.forEach(item => {
+      if (typeof item === "string") {
+        result.push(item.replace(/\s/g, "")[0].toUpperCase());
+      }
+    });
+    return result.sort().join("");
+  } else return false;
   // remove line with error and write your code here
 };

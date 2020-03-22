5e8b525612da72708da8f1feb3390c7175d9c24e
Masha <valzyr48@gmail.com>
2020-03-22 13:37:02 +0300

    feat: add solution for recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,43 +1,14 @@
 module.exports = class DepthCalculator {
-  constructor() {
-    this.i = 1;
-    this.calculateDepth = this.calculateDepth.bind(this);
-  }
-  calculateDepth(arr) {
-    // arr.forEach(item => {
-    //   if (item instanceof Array) {
-    //     this.i += 1;
-    //     calculateDepth(item);
-    //   }
-    // });
-    // return this.i;
-
-    if (item instanceof Array) {
-      return (this.i += this.calculateDepth(item));
-    } else {
-      return this.i;
+  calculateDepth = (arr, depth = 1) => {
+    let depthOfArr = depth;
+    for (let i = 0; i < arr.length; ++i) {
+      if (Array.isArray(arr[i])) {
+        const Depth = this.calculateDepth(arr[i], depth + 1);
+        if (Depth > depthOfArr) {
+          depthOfArr = Depth;
+        }
+      }
     }
-  }
+    return depthOfArr;
+  };
 };
-
-//class DepthCalculator {
-//   constructor() {
-//     this.i = 0;
-//     this.calculateDepth = this.calculateDepth.bind(this);
-//   }
-//   calculate(mas) {
-//     let i = 0;
-//     return this.calculateDepth(mas);
-//   }
-//   calculateDepth(arr) {
-//     this.i += 1;
-//     arr.forEach(item => {
-//       if (item instanceof Array) {
-//         this.i += 1;
-//         calculateDepth(item);
-//       }
-//     });
-//     return this.i;
-//     // remove line with error and write your code here
-//   }
-// };

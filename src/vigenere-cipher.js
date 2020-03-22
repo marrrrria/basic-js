0547f68b7d2d9c65b08afd9e6de4a899f6e641bf
Masha <valzyr48@gmail.com>
2020-03-22 14:20:22 +0300

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,62 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  constructor(direct = true) {
+    this.isDirect = direct;
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
     }
+    const KEY = [...key];
+    const MESSAGE = [...message];
+    while (KEY.length < MESSAGE.length) {
+      KEY.push(...KEY);
+    }
+    const SOLUTION = MESSAGE.map(item => {
+      return /[a-z]/i.test(item)
+        ? String.fromCharCode(
+            ((item.toUpperCase().charCodeAt() -
+              65 +
+              KEY.shift()
+                .toUpperCase()
+                .charCodeAt() -
+              65) %
+              26) +
+              65
+          )
+        : item;
+    });
+    return this.isDirect ? SOLUTION.join("") : SOLUTION.reverse().join("");
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error();
+    }
+    const KEY = [...key];
+    const MESSAGE = [...message];
+    while (KEY.length < MESSAGE.length) {
+      KEY.push(...KEY);
+    }
+    const SOLUTION = MESSAGE.map(item => {
+      if (/[a-z]/i.test(item)) {
+        const decriptedLetter =
+          (item.toUpperCase().charCodeAt() -
+            65 -
+            KEY.shift()
+              .toUpperCase()
+              .charCodeAt() -
+            65) %
+          26;
+        return String.fromCharCode(
+          (decriptedLetter < 0 ? decriptedLetter + 26 : decriptedLetter) + 65
+        );
+      } else {
+        return item;
+      }
+    });
+    return this.isDirect ? SOLUTION.join("") : SOLUTION.reverse().join("");
+  }
 }
 
 module.exports = VigenereCipheringMachine;
